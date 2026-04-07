878a709f98fa6cce2bba6595a190fe1d721d0721
cezar <cezarcoh@gmail.com>
2026-04-06 22:06:39 -0500

    creando tablas

--- django_docker/core/settings.py
+++ django_docker/core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 # apps de terceros ..... tambien cambie
     'rest_framework', 
+    'tareas', 
 ]
 
 MIDDLEWARE = [
---
+++ django_docker/tareas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Tarea  
+
+admin.site.register(Tarea) # registrar el modelo Tarea para que aparezca en el admin
+
+# Register your models here.
